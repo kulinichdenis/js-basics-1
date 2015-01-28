1cc49820f191a245a7ea42d1d7bf306f5a0ad48a
kulinichdenis <kulinichdenis@ukr.net>
2015-01-28 15:16:49 +0200

    solution

--- solution.js
+++ solution.js
@@ -1,9 +1,36 @@
 function squaresOfMax(a, b, c) {
+
+    function(a,b,c){
+    if(a>c && b>c){
+        return (a*a)+(b*b);
+    }
+    if(b>=a && c>a){
+        return (b*b)+(c*c);
+    }
+    if(a>=b && c>=b){
+        return (a*a)+(c*c);
+    }
+
   // Write a function that takes three numbers and returns
   // sum of squares of two largest
 }
 
 function drawTriangle() {
+
+    var i=0
+    var k = 0;
+    var string="";
+    while(i<8){
+        string+='\n';
+        while(k<i){
+            string+='#';
+            k++;
+        }
+        k=0;
+        i++;
+    }
+    console.log(string);
+
   // Write a loop that makes seven calls to console.log to output the following triangle:
   //
   // #
@@ -16,6 +43,20 @@ function drawTriangle() {
 }
 
 function fizzBuzz() {
+
+    for(var i=1; i<=100; i++){
+        if(!(i%3)){
+            console.log('Fizz');
+        }
+        if(!(i%5)){
+            console.log('Buzz');
+        }
+        if((!(i%3) && !(i%5))){
+            console.log('FizzBuzz');
+        }
+        console.log(i);
+    }
+
   // Write a program that uses console.log to print all the numbers from 1 to 100,
   // with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number,
   // and for numbers divisible by 5 (and not 3), print "Buzz" instead.
@@ -26,6 +67,31 @@ function fizzBuzz() {
 }
 
 function chessBoard(size) {
+	
+    if(size === 0){return "";}
+    if(size === 1){return " ";}
+
+    var sizeInner = size || 8;
+    var string="";
+    var flag = true;
+
+    for(var i = 0; i < sizeInner; i++){
+        var firstFlag = flag;
+//        debugger
+        for(var j=0; j<sizeInner; j++){
+            if(flag){
+                string +=" ";
+                flag = false;
+            }else{
+                string +='#';
+                flag = true;
+            }
+        }
+        string += '\n';
+        flag = !firstFlag;
+    }
+
+    return string;
   // Write a program that returns a string that represents an 8×8 grid,
   // using newline characters to separate lines. At each position of the
   // grid there is either a space or a “#” character. The characters
